4c0a172e37230ea53161697e7f1e4f68e339898f
Hyunjoon Kim <joonkim0625@gmail.com>
2018-09-13 17:26:50 +0900

    수업 내용 정리

---
+++ js/transition.js
@@ -0,0 +1,6 @@
+var box = $('.box1');
+var btn = $('.box-ani');
+
+btn.click(function () {
+  box.toggleClass('box-act');
+});
--- js/webcafe.js
+++ js/webcafe.js
@@ -1,1 +1,16 @@
 // jQuery를 활용한 인터렉션 ...
+
+var item = $('.menu-item');
+var subItem = $('.sub-menu a');
+item.attr('tabindex', 0);
+subItem.attr('class', 'fas fa-angle-left');
+
+// .on => 멀티 이벤트를 가능케 함
+ item.on('mouseover focusin', function(){
+   item.removeClass('menu-act');
+   $(this).addClass('menu-act');
+ }); 
+
+ subItem.hover(function(){
+   $(this).toggleClass('fa-angle-right');
+ });
